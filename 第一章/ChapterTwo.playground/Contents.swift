0780021c01406651bd0f542b15a157cbbb000919
shavekevin <shavekevin@gmail.com>
2018-06-01 18:08:37 +0800

    基础部分(待完善)

---
+++ 第一章/ChapterTwo.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+//: author:shavekevin  https://github.com/shaveKevin
+import UIKit
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempts  = 0
+var x = 0.0,y = 0.0,z = 0.0
+
+var welcomeMessage: String
+welcomeMessage = "Hello"
+var red,green,blue:Double
+// 常量和变量的命名
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐂 = "dogcow"
+
