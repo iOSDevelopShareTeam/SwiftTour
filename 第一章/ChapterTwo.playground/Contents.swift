b05dd83d1312f7b69dad781e7cf2cc4ec5d51e6b
shavekevin <shavekevin@gmail.com>
2018-06-07 18:02:09 +0800

    基础部分(基本数据类型，待完善)

--- 第一章/ChapterTwo.playground/Contents.swift
+++ 第一章/ChapterTwo.playground/Contents.swift
@@ -35,7 +35,7 @@ print("minValue of UInt8 is \(minValue) and maxValue of UInt8 is \(maxValue)。"
 let meaningOfLife = 42
 // meaningOfLife will be inferred to be of  type Int  这里常量meaningOfLife 会被便以及推断为一个整型
 let  pi = 3.14159
-// pi will be inferred to be of type Double 这里常量pi会被默认推断为DoubleL类型的数据。
+// pi will be inferred to be of type Double 这里常量pi会被默认推断为Double类型的数据。
 
 let anotherPi = 3 + 0.14159
 // anotherPi will be inferred of type Double 这里常量anotherPi会被swift推断为Double类型的数据。
@@ -43,6 +43,24 @@ let decimalInteger = 17
 let binaryInteger  = 0b10001// 二进制的17 2*2*2*2+1
 let octalInteger = 0o21 // 八进制的17 2*8+1*1
 let hexadecimalInteger = 0x11 // 十六进制的17 16*1+1
+//浮点类型的十进制的12.1875
+let decimalDouble = 12.1875
+let exponentDouble = 1.21875e1
+let hexadecimalDouble = 0xC.3p0
+/*0x代表16进制C代表12后面.3代表小数 所以整数部分应该是应该是 12*(16^0)+0.3*(16^-1)后面的p代表指数，以2为底。所以完整表达式为
+（12*(16^0)+0.3*(16^-1)）*(2^0)
+ */
+let paddedDouble = 000123.456
+let oneMillion = 1_000_000
+let justOverOneMillion = 1_000_000.000_000_000_1
+
+// 注意常量或者变量如果超过范围那么会报错
+//let cannotBeNegative:UInt8 = -1 //不在UInt8值范围内报错
+//let tooBig:Int8 = Int8.max + 1  //超出Int8值范围内报错
+
+let twoThousand:UInt16 = 2_000
+let one:UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
 
 
 
