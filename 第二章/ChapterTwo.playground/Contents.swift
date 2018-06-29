294f9af98b8ef71d825c0d7c10cded9947d4f4ce
shavekevin <shavekevin@gmail.com>
2018-06-29 15:49:01 +0800

    基本运算符(待完善)

--- 第二章/ChapterTwo.playground/Contents.swift
+++ 第二章/ChapterTwo.playground/Contents.swift
@@ -310,10 +310,47 @@ var bbb = 6
 aaa != nil ? aaa!  : bbb
 let defaultColorName = "red"
 var userDefinedColorName: String?//默认值是nil
+userDefinedColorName = "green"
 var colorNameToUse = userDefinedColorName ?? defaultColorName
 // 因为userDefinedColorName是nil，所以colorNameToUse被设置为默认值"red"
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
+//1 times 5 is 5
+//2 times 5 is 10
+//3 times 5 is 15
+//4 times 5 is 20
+//5 times 5 is 25
 
+let names = ["Anna","Alex","Brian","Jack"]
+let count = names.count
+for i in 0 ..< count {
+    print("Person \(i + 1) is called \(names[i])")
+}
+// 打印结果是：
+//Person 1 is called Anna
+//Person 2 is called Alex
+//Person 3 is called Brian
+//Person 4 is called Jack
+for name in names[2...] {
+    print("\(name)")
+}
+// 打印结果是：
+//  Brian
+// Jack
+for name in names[...2] {
+    print("\(name)")
+}
+// 打印结果是：
+//  Anna
+//  Alex
+// Brian
 
-
+for name in names[..<2] {
+    print("\(name)")
+}
+// 打印结果是：
+//Anna
+//Alex
 
 
